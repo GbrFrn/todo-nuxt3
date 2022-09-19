a801f49ed05bd0a5fc1b21253fa2759a998bef73
Gabriel Fernandes <gabrielvmfernandes@gmail.com>
2022-09-19 17:49:31 -0300

    inital commit

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <NuxtWelcome class=" max-w-full" />
+  </div>
+</template>
---
+++ store/todo.ts
@@ -0,0 +1,71 @@
+import { defineStore } from "pinia";
+import { v4 as uuid } from "uuid";
+
+//Typescript interfaces and types
+
+export interface Todo {
+  id: string;
+  title: string;
+  done: boolean;
+  createAt: Date;
+  updatedAt: Date;
+}
+
+export interface TodoState {
+  items: Todo[] | undefined[];
+}
+
+export interface TodoAdd {
+  title: string;
+}
+
+export interface TodoUpdate {
+  title?: string;
+  done?: boolean;
+}
+
+//States
+
+function state(): TodoState {
+  return {
+    items: [],
+  };
+}
+
+const getters = {
+  getById: (state: TodoState) => (id: string) => {
+    return state.items.find((item: Todo) => item.id === id);
+  },
+  getOrderedTodos: (state: TodoState) =>
+    state.items.sort(
+      (a: Todo, b: Todo) =>
+        a.createAt.getMilliseconds() - b.createAt.getMilliseconds()
+    ),
+};
+
+const actions = {
+  add(partialTodo: TodoAdd) {
+    const todo: Todo = {
+      id: uuid(),
+      ...partialTodo,
+      done: false,
+      createAt: new Date(),
+      updatedAt: new Date(),
+    };
+    this.items.push(todo);
+  },
+  remove(id: string) {
+    this.items = this.items.filter((todo: Todo) => todo.id !== id);
+  },
+  update(id: string, update: TodoUpdate) {
+    this.items = this.items.map((item) =>
+      item.id === id ? { ...item, ...update, updatedAt: new Date() } : item
+    );
+  },
+};
+
+export const useTodoStore = defineStore("todoStore", {
+  state,
+  getters,
+  actions,
+});
